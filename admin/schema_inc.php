5c7c16c75094079162c7eb8bf42e2a59f75b3442
tekimaki <will@tekimaki.com>
2010-08-27 14:56:40 -0400

    isolate installer requirements to run only at install time

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -1,20 +1,27 @@
 <?php
 
-global $gBitInstaller;
+global $gBitSystem;
 
-$gBitInstaller->registerPackageInfo( MODCOMMENTS_PKG_NAME, array(
+$gBitSystem->registerPackageInfo( MODCOMMENTS_PKG_NAME, array(
 	'description' => 'Allow admins or content creators to moderate comments.',
 	'requirements' => 'ModComments is dependent on <a class="external" href="http://www.bitweaver.org/wiki/moderationpackage">ModerationPackage</a>',
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
 ) );
+
+// Requirements
+$gBitSystem->registerRequirements( MODCOMMENTS_PKG_NAME, array(
+    'liberty' => array( 'min' => '2.1.4' ),
+));
+
+// Install process
+global $gBitInstaller;
+if( is_object( $gBitInstaller ) ){
 
 // ### Default Preferences
 $gBitInstaller->registerPreferences( MODCOMMENTS_PKG_NAME, array(
 //	array( NEXUS_PKG_NAME, 'nexus_menu_text', 'Menus' ),
 ) );
 
-// Requirements
-$gBitInstaller->registerRequirements( MODCOMMENTS_PKG_NAME, array(
-    'liberty' => array( 'min' => '2.1.4' ),
-));
+}
+
 
